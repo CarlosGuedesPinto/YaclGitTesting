a960926afe008f02e4d05467a4eecc48d8af7aa0
carlosguedespinto <cguedes@infraspeak.com>
2021-09-09 15:31:10 +0100

    first commit

---
+++ .yalc/package_npm_link_test/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeter = void 0;
+var Greeter = function (name) { return "Hello " + name; };
+exports.Greeter = Greeter;
