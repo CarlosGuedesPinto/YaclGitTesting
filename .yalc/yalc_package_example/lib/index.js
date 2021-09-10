82a6f586607439304d908344a31278ed1cf0c565
carlosguedespinto <cguedes@infraspeak.com>
2021-09-10 14:41:56 +0100

    1.0.2

---
+++ .yalc/yalc_package_example/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeter = void 0;
+var Greeter = function (name) { return "Hello Ana Beautiful and Carlos Amazing 2 " + name; };
+exports.Greeter = Greeter;
